36f47ea498134365e8a533d456c4378c28c3682a
Adam Hall <adam.ralph.hall@gmail.com>
2023-06-27 22:39:56 -0400

    mobile fixing

--- script.js
+++ script.js
@@ -30,72 +30,65 @@ function preload() {
 
 function create() {
 
-  this.input.setPollAlways();
+    this.input.setPollAlways();
 
-  background = this.add.image(
+    background = this.add.image(
     canvasWidth / 2,
     canvasHeight / 2,
     "background"
-  );
-  let georgeRatio = 412/605
-  let georgeWidth = 109
+    );
 
-  background.setDisplaySize(canvasWidth, canvasHeight);
-  george = this.physics.add.sprite(
+    background.setDisplaySize(canvasWidth, canvasHeight);
+    george = this.physics.add.sprite(
     800 / 10,
     500 / 2,
     "george"
-  );
-  george.setScale(1.2)
-  george.setSize(george.displayWidth - 300, george.displayHeight - 400)
-//  george.setSize(412/3, 605/3) 
-  george.setFrame(1);
-  george.setBounce(.75, 0.75);
-  george.setCollideWorldBounds(true);
+    );
+    george.setScale(1.2)
+    george.setSize(george.displayWidth - 300, george.displayHeight - 400)
+    //  george.setSize(412/3, 605/3) 
+    george.setFrame(1);
+    george.setBounce(.75, 0.75);
+    george.setCollideWorldBounds(true);
 
 
-  georgeHead = this.physics.add.existing(this.add.zone(george.x, george.y, 100, 100))
-  georgeHead.body.setAllowGravity(false)
+    georgeHead = this.physics.add.existing(this.add.zone(george.x, george.y, 100, 100))
+    georgeHead.body.setAllowGravity(false)
 
 
 
-  spat = this.physics.add.image(
+    spat = this.physics.add.image(
     800 / 2,
     500 / 2,
     "spat"
-  );
-  spat.setScale(.5)
-  spat.setBounce(0)
-  spat.body.setAllowGravity(false)
-  spatHead = this.physics.add.existing(this.add.zone(spat.x, spat.y, 140, 160))
-  spatHead.body.setAllowGravity(false)
+    );
+    spat.setScale(.5)
+    spat.setBounce(0)
+    spat.body.setAllowGravity(false)
+    spatHead = this.physics.add.existing(this.add.zone(spat.x, spat.y, 140, 160))
+    spatHead.body.setAllowGravity(false)
 
-  this.physics.add.collider(george, spat);
+    this.physics.add.collider(george, spat);
 
 
-  this.physics.add.overlap(spatHead, george, reflect, null, this);
+    this.physics.add.overlap(spatHead, george, reflect, null, this);
 
-  food = this.physics.add.group()
+    food = this.physics.add.group()
 
-  deleteZone1 = this.physics.add.existing(this.add.zone(canvasWidth/2, canvasHeight + 1000, canvasWidth*2, 200))
-  deleteZone2 = this.physics.add.existing(this.add.zone(-1000, canvasHeight/2, 200, canvasHeight*2))
-  deleteZone3 = this.physics.add.existing(this.add.zone(canvasWidth + 1000, canvasHeight/2, 200, canvasHeight*2))
-  deleteZone1.body.moves = false
-  deleteZone2.body.moves = false
-  deleteZone3.body.moves = false
+    deleteZone1 = this.physics.add.existing(this.add.zone(canvasWidth/2, canvasHeight + 1000, canvasWidth*2, 200))
+    deleteZone2 = this.physics.add.existing(this.add.zone(-1000, canvasHeight/2, 200, canvasHeight*2))
+    deleteZone3 = this.physics.add.existing(this.add.zone(canvasWidth + 1000, canvasHeight/2, 200, canvasHeight*2))
+    deleteZone1.body.moves = false
+    deleteZone2.body.moves = false
+    deleteZone3.body.moves = false
 
-  deleteZone = this.physics.add.group([deleteZone1, deleteZone2, deleteZone3])
-  
+    deleteZone = this.physics.add.group([deleteZone1, deleteZone2, deleteZone3])
 
-  this.physics.add.overlap(deleteZone, food, deleteFood, null, this);
-  this.physics.add.overlap(georgeHead, food, eatFood, null, this);
-  this.physics.add.overlap(spatHead, food, reflect, null, this)
-    //   food.createMultiple({key: ['bagel', 'pizza', 'banana', 'teq', 'uncrust'], quanity: 5 })
-    //   food.children.iterate((child) => {
-    //     child.setScale(.4);
-    //     child.body.moves = false
-    //   });
-    //   Phaser.Actions.SetXY(food.getChildren(), 32, 100, 300)
+
+    this.physics.add.overlap(deleteZone, food, deleteFood, null, this);
+    this.physics.add.overlap(georgeHead, food, eatFood, null, this);
+    this.physics.add.overlap(spatHead, food, reflect, null, this)
+
     this.physics.add.collider(food, food);
     this.physics.add.collider(food, george);
 
@@ -117,7 +110,6 @@ function create() {
             loop: true
         })
             
-    music.play()
 
 
     // this.input.on('pointerdown', function (pointer) {
@@ -135,27 +127,19 @@ function update() {
         
     if (!clicked && this.input.activePointer.isDown) {
         clicked = true
+        music.play() //possibly stop clicked from being true if music isn't laoded
     }
-
- 
-
-   
 
     georgeHead.body.x = george.body.x + george.width/2 - 130;
     georgeHead.body.y = george.body.y - 100;
 
     if (clicked){  
-    spat.body.x = this.input.activePointer.x - spat.displayWidth + 15
-    spat.body.y = this.input.activePointer.y - spat.displayHeight + 17}
-
+    spat.body.x = this.input.activePointer.worldX - spat.displayWidth + 15
+    spat.body.y = this.input.activePointer.worldY - spat.displayHeight + 17
+    }
 
     spatHead.body.x = spat.body.x + 1;
     spatHead.body.y = spat.body.y;
-
- 
-
-    // spat.rotation += 0.01;
-
 }
 
 function createFood(object, food) {
